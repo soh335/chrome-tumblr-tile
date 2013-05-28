7a3f387703c52a5f94b0c9725d9727b807a975b3
pastak <pasta0915@gmail.com>
2013-05-28 19:15:12 +0900

    add Link to Common tab page

--- newTab.js
+++ newTab.js
@@ -1,3 +1,8 @@
 $(function() {
     tumblrTile.draw();
+	a=document.getElementById("commonlink");
+a.addEventListener("click",function(){
+	console.log("aaa");
+	chrome.tabs.update( {url:"chrome-internal://newtab/"} );
+},true);
 });
